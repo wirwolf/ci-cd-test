2b702edaf99dd796037ac07d5dee859ebb439b95
Andru Cherny <wiroatom@gmail.com>
2023-08-10 11:14:01 +0300

    test

---
+++ .ci-cd/install-cluster.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+. common-cicd.sh
+set -eu
+
+function install_kubernetes() {
+  install_snap microk8s --stable --classic
+
+  # export the kubectl config file in case other tools rely on this
+  mkdir -p $HOME/.kube
+#  microk8s.kubectl config view --raw > $HOME/.kube/config
+  echo "Waiting for kubernetes core services to be ready.."
+  microk8s.status --wait-ready
+  sudo snap alias microk8s.kubectl kubectl
+}
+
+function install_addons() {
+  microk8s.enable dns
+  microk8s.enable dashboard
+  microk8s.enable ingress
+  microk8s.enable storage
+
+  # Ensure kubernetes is running properly
+  echo "Checking kubernetes addons are running.."
+
+  until [[ `kubectl get pods --all-namespaces | grep -o 'ContainerCreating' | wc -l` == 0 ]] ; do
+    echo "Waiting for kubernetes addon service pods to be ready, sleeping 10s  ("`kubectl get pods --all-namespaces | grep -o 'ContainerCreating' | wc -l`" not running)"
+    sleep 10
+  done
+}
+
+##
+# Ensure KUBECONFIG is in .bashrc so that other kubernetes tools can use it
+# NB: No longer needed (ensuring default lcoation ~/.kube/config instaed).. not used
+#
+function ensure_kubeconfig() {
+  grep KUBECONFIG ${HOME}/.bashrc > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "Skipping adding KUBECONFIG to ~/.bashrc, already exists"
+  else
+    echo "Adding KUBECONFIG to ~/.bashrc"
+    printf "\n\nexport KUBECONFIG=/snap/microk8s/current/client.config\n\n" >> ${HOME}/.bashrc
+  fi
+}
+
+ensure_root
+install_kubernetes
+install_addons
+
+#microk8s.kubectl config view --raw > $HOME/.kube/config
+#sudo snap alias microk8s.kubectl kubectl
